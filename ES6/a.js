dda4a71f9ac8b4c31c0d303b73ea88f041620fc3
kewen <957314724@qq.com>
2017-05-17 10:22:17 +0800

    es6

---
+++ ES6/a.js
@@ -0,0 +1,6 @@
+console.log('kv');
+alert('kv');
+console.log('zj');
+for(var a=0;a<1;a++){
+	console.log('kv');
+}
---
+++ ES6/a.min_2.js
@@ -0,0 +1,5 @@
+!function(o){function r(n){if(e[n])return e[n].exports;var t=e[n]={exports:{},id:n,loaded:!1};return o[n].call(t.exports,t,t.exports,r),t.loaded=!0,t.exports}var e={};return r.m=o,r.c=e,r.p="",r(0)}([/*!**************!*\
+  !*** ./a.js ***!
+  \**************/
+function(o,r){console.log("kv"),alert("kv"),console.log("zj");for(var e=0;1>e;e++)console.log("kv")}]);
+//# sourceMappingURL=a.min_2.js.map
